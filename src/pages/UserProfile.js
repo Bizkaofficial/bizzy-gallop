c6e00d5f9c949bd7770ebec9ddc618beffdc5912
Larryking007 <ajikobilanre007@gmail.com>
2022-06-10 22:28:28 +0100

    profile images

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -33,18 +33,39 @@ const UserProfile = () => {
           </a>
         </div>
         <div className="r_side col">
-          <h1>About ME</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />{" "}
-            Aliquet viverra etiam diam interdum suscipit velit integer. <br />{" "}
-            Imperdiet leo nunc nulla volutpat phasellus. Nunc <br /> suspendisse
-            ac facilisi eget quis at porta. Iaculis facilisi et <br /> vel, ut.
-            Id volutpat, aliquet convallis elementum ornare <br /> turpis. Eu
-            maecenas cras euismod nisi, dictum. Pretium.
-          </p>
-          <button>Add Project</button>
+          <div className="article">
+            <h1>About ME</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />{" "}
+              Aliquet viverra etiam diam interdum suscipit velit integer. <br />{" "}
+              Imperdiet leo nunc nulla volutpat phasellus. Nunc <br />{" "}
+              suspendisse ac facilisi eget quis at porta. Iaculis facilisi et{" "}
+              <br /> vel, ut. Id volutpat, aliquet convallis elementum ornare{" "}
+              <br /> turpis. Eu maecenas cras euismod nisi, dictum. Pretium.
+            </p>
+            <button className="article_btn">Add Project</button>
+          </div>
+          <div className="container-fluid p-4">
+            <h4>My Projects</h4>
+            <div className="card-group">
+              <div className="card col-4 m-4">
+                <img src="" className="card-img-top" />
+              </div>
+              <div className="card col-6 m-6">
+                <img src="" className="card-img-top" />
+              </div>{" "}
+              <br />
+              <div className="card col-6 m-6">
+                <img src="" className="card-img-top" />
+              </div>
+              <div className="card col-6 m-6">
+                <img src="" className="card-img-top" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <div className="background-primary bottom"></div>
     </div>
   );
 };
