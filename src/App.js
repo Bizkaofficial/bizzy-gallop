3221fb74b1620408a6206d7076721173902f964d
Larryking007 <ajikobilanre007@gmail.com>
2022-07-03 23:19:31 +0100

    blog post

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from "./routes/User";
 import About from "./pages/About";
 import UserProfile from "./pages/UserProfile";
 import ProjectUpload from "./pages/ProjectUpload";
+import Blog  from "./pages/Blog";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           caseSensitive="false"
           element={<UserProfile />} />
         <Route  path="/projectupload" caseSensitive="fasle" element={<ProjectUpload />} />
+        <Route path="/blog" caseSensitive="false" element={<Blog />} />
       </Routes>
     </>
   );
