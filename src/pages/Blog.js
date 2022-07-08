4d5d27b129312b2eab44aa8c1957ebab3b8eef06
Larryking007 <ajikobilanre007@gmail.com>
2022-07-08 09:29:09 +0100

    search bar

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,12 +2,13 @@ import Frame47 from "../assets/Frame47.png";
 import Frame48 from "../assets/Frame48.png";
 import Frame49 from "../assets/Frame49.png";
 import Frame50 from "../assets/Frame50.png";
+import Frame51 from "../assets/Frame51.png";
 
 const Blog = () => {
   return (
-    <div className="container-fluid">
-      <div className="d-flex">
-        <div>
+    <div className="container-fluid ml-2">
+      <div className="row mx-3">
+        <div className="col-md-5">
           <h1 className="mb-4">Data is Life</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices, dis
@@ -16,29 +17,76 @@ const Blog = () => {
             accumsan lobortis penatibus et odio tempus, suscipit
           </p>
         </div>
-        <div>
+        <div className="col-md-5 mx-3">
           <img src={Frame47} alt="" width={575} height={250} />
-          <p>
+          <p className="h5 mx-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
           </p>
         </div>
       </div>
-      <div className="d-flex">
+      <div className="row">
+        <div className="col-md-5">
         <img src={Frame48} alt="" width={655} height={405} />
-        <div className="pt-4">
+        </div>
+        <div className="col-md-5 mx-3">
           <img src={Frame49} alt=" " width={575} height={245} />
-          <p>
+          <p className="h5 mx-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
           </p>
         </div>
       </div>
-      <h4>Latest Posts</h4>
+      <h4 className="mx-3">Latest Posts</h4>
       <hr />
-      <div className="d-flex">
-        <img src={Frame50} width={575} height={250} alt="" />
-        <h4>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
-        </h4>
+      <div className="row">
+        <div className="col-md-5 mx-3">
+          <img src={Frame50} width={575} height={250} alt="" />
+          <h4>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices, dis
+            pulvinar purus ultricies posuere nisi justo, placerat cum
+            pellentesque primis tempus accumsan aenean. Cras mattis sociosqu
+            accumsan lobortis penatibus et odio tempus, suscipit
+          </p>
+          <img src={Frame51} width={575} height={245} />
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices, dis
+            pulvinar purus ultricies posuere nisi justo, placerat cum
+            pellentesque primis tempus accumsan aenean. Cras mattis sociosqu
+            accumsan lobortis penatibus et odio tempus, suscipit
+          </p>
+        </div>
+        <div className="col-md-4 mx-3">
+          <div className="input-group offset-md-4">
+            <input
+              type="text"
+              className="form-control-lg border round"
+              placeholder="Search bar"
+              aria-label="Search bar"
+              aria-describedby="basic-addon2"
+            />
+            <span className="input-group-text bg-primary" id="basic-addon2">
+              @
+            </span>
+          </div>
+          <div className="post--text align-self-center bg-primary my-5 lh-lg text-light px-5 py-5">
+            <p className="fs-4">
+              Register for free to receive relevant updates on latest Data
+              Analysis
+            </p>
+            <p>Create an account to receive our newsletter and promotions.</p>
+            <button
+              type="button"
+              class="btn btn-light text-primary offset-md-5"
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -4,82 +4,75 @@ import UserProfileImg2 from "../assets/userprofile2.png";
 import UserProfileImg3 from "../assets/userprofile3.png";
 import UserProfileImg4 from "../assets/userprofile4.png";
 import UserProfileImg5 from "../assets/userprofile5.png";
+
 const UserProfile = () => {
   return (
     <div className="container-fluid">
       <div className="top"></div>
-      <div className="user_profile">
-        <div className=" aside text-center">
-          <img className="userprofileimg1" src={UserProfileImg1} alt=""></img>
+      <div className="user_profile row">
+        <div className="col-md-4 aside text-center">
+          <img className="userprofileimg1" src={UserProfileImg1} alt="" />
           <h4>Name</h4>
           <h4>Headlines</h4>
-          <a className="row" href="#" alt="">
+          <a href="#" alt="">
             Personal Details
-          </a>
-          <a className="row" href="#" alt="">
+          </a> <br />
+          <a href="#" alt="">
             Professional Summary
-          </a>
-          <a className="row" href="#" alt="">
-            Service Tools{" "}
-          </a>
-          <a className="row" href="#" alt="">
-            {" "}
+          </a> <br />
+          <a href="#" alt="">
+            Service Tools
+          </a> <br />
+          <a href="#" alt="">
             Work Experience
-          </a>
-          <a className="row" href="#" alt="">
+          </a> <br />
+          <a href="#" alt="">
             Education
-          </a>
-          <a className="row" href="#" alt="">
+          </a> <br />
+          <a href="#" alt="">
             Course Certificate
-          </a>
-          <a className="row" href="#" alt="">
+          </a> <br />
+          <a href="#" alt="">
             Contact Info
-          </a>
+          </a> <br />
         </div>
-        <div className="r_side">
+        <div className="col-md-5 offset-md-1 r_side">
           <div className="article">
             <h1>About ME</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />{" "}
-              Aliquet viverra etiam diam interdum suscipit velit integer. <br />{" "}
-              Imperdiet leo nunc nulla volutpat phasellus. Nunc <br />{" "}
-              suspendisse ac facilisi eget quis at porta. Iaculis facilisi et{" "}
-              <br /> vel, ut. Id volutpat, aliquet convallis elementum ornare{" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+              Aliquet viverra etiam diam interdum suscipit velit integer. <br />
+              Imperdiet leo nunc nulla volutpat phasellus. Nunc <br />
+              suspendisse ac facilisi eget quis at porta. Iaculis facilisi et
+              <br /> vel, ut. Id volutpat, aliquet convallis elementum ornare
               <br /> turpis. Eu maecenas cras euismod nisi, dictum. Pretium.
             </p>
-            <button className="article_btn">Add Project</button>
+            <button className="float-end">Add Project</button>
           </div>
-          <div className="p-4">
-            <h4>My Projects</h4>
-            <div className="card-group p-4 row">
-              <div className=" col-lg-4 m-4">
-                <img
-                  className="card-img-top"
-                  src={UserProfileImg2}
-                  width={100}
-                  height={170}
-                  alt=""
-                />
+          <div className="project-banner">
+            <h4 className="mt-5">My Projects</h4>
+            <div className="card-deck row">
+              <div className="card col-lg-5 mx-3 my-3">
+                <img className="card-img-top" src={UserProfileImg2} alt="Card image cap" />
               </div>
-              <div className="col-lg-4 m-4">
-                <img className="card-img-top" src={UserProfileImg3} />
-              </div>{" "}
-              <br />
-              <div className="col-lg-4 m-4">
-                <img className="card-img-top" src={UserProfileImg4} />
+              <div className="card col-md-5 mt-3 mb-3">
+                <img className="card-img-top" src={UserProfileImg3} alt="Card image cap" />
               </div>
-              <div className=" col-lg-4 m-4">
-                <img
-                  className="card-img-top"
-                  src={UserProfileImg5}
-                  width={250}
-                />
+              <div className="card col-lg-5 mx-3 my-3">
+                <img className="card-img-top" src={UserProfileImg4} alt="Card image cap" />
+              </div>
+              <div className="card border col-lg-5 my-3">
+                <img className="card-img-top" src={UserProfileImg5} alt="Card image cap" />
+              </div>
+              <div className="card col-lg-5 my-3 bg-success">
               </div>
             </div>
           </div>
-        </div>
-      </div>
+          </div>
       <div className="background-primary bottom"></div>
+    </div>
+    <div className="project--footer bg-primary" width={1430} heigth={446}>
+    </div>
     </div>
   );
 };
