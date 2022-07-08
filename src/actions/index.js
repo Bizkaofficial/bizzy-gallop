4171b01b90376f32fe1efc7b36100850e857f3f0
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-08 12:51:36 +0100

    auth/register api not working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
+import axios from "axios"
+
 const registerUser = (user) => {
-    return {
-        type: 'REGISTER_USER',
-        payload: user
+    return (dispatch)=>{
+        return axios.post('https://bizka.onrender.com/auth/register', user).then((res)=>{
+            console.log(res)
+            dispatch({type: 'REGISTER_USER', payload: {response: res, error: ''}})
+        }).catch((err)=>{
+            console.log(err)
+            dispatch({type: 'REGISTER_USER', payload: {response: '', error: err.message}})
+        })
     }
 }
 
