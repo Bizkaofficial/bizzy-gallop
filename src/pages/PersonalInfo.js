83ce0bdd3b432d2f04b812334884b20e4692a6d3
Larryking007 <ajikobilanre007@gmail.com>
2022-07-16 19:52:47 +0100

    form image

--- src/pages/PersonalInfo.js
+++ src/pages/PersonalInfo.js
@@ -1,9 +1,64 @@
+import Ellipse4 from "../assets/Ellipse4.png";
+import Ellipse5 from "../assets/Ellipse5.png";
 
 const PersonalInfo = () => {
-    return(
-        <section>
-
-        </section>
-    )
-}
+  return (
+    <section>
+      <button type="button" className="btn btn-primary">
+        Back to Profile
+      </button>
+      <div className="row">
+        <div className="col">
+          <p className="text-warning">Personal details</p>
+        </div>
+        <div className="col bg-light rounded-start rounded-end">
+          <form>
+            <img
+              src={Ellipse4}
+              width={178}
+              height={191}
+              className="rounded-circle"
+              alt=""
+            />
+            <img src={Ellipse5} alt="" />
+            <div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First Name"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last Name"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Location"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Headline"
+              />
+            </div>
+            <div>
+              <textarea
+                rows="5"
+                className="form-control textarea-input"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default PersonalInfo;
--- src/pages/SocialMedia.js
+++ src/pages/SocialMedia.js
@@ -5,12 +5,12 @@ const SocialMedia = () => {
         <h2 className="display-5 m-5">Online Presence</h2>
         <div className="row">
           <div className="col-9">
-            <div className="row">
+            <div className="row mb-4">
               <div className="col-3 mx-5">
                 <p>Linked In</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">
+                <button type="button" className="btn btn-secondary rounded-pill">
                   add link
                 </button>
               </div>
@@ -20,14 +20,14 @@ const SocialMedia = () => {
             <p>Custom Link</p>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Twitter</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">
+                <button type="button" className="btn btn-secondary rounded-pill">
                   add link
                 </button>
               </div>
@@ -37,14 +37,14 @@ const SocialMedia = () => {
             <p>Link Title</p>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Facebook</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">
+                <button type="button" className="btn btn-secondary rounded-pill">
                   add link
                 </button>
               </div>
@@ -54,14 +54,14 @@ const SocialMedia = () => {
             <p>Enter URL</p>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Youtube</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">
+                <button type="button" className="btn btn-secondary rounded-pill">
                   add link
                 </button>
               </div>
@@ -71,76 +71,76 @@ const SocialMedia = () => {
             <p>ADD</p>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Pinterest</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">
+                <button type="button" className="btn btn-secondary rounded-pill">
                   add link
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Instagram</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">add link</button> 
+                <button type="button" className="btn btn-secondary rounded-pill">add link</button> 
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Medium</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">add link</button> 
+                <button type="button" className="btn btn-secondary rounded-pill">add link</button> 
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Github</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">add link</button> 
+                <button type="button" className="btn btn-secondary rounded-pill">add link</button> 
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Wordpress</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">add link</button> 
+                <button type="button" className="btn btn-secondary rounded-pill">add link</button> 
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col-9">
             <div className="row">
               <div className="col-3 mx-5">
                 <p>Telegram</p>
               </div>
               <div className="col-6">
-                <button type="button" className="btn btn-light rounded-pill">add link</button> 
+                <button type="button" className="btn btn-secondary rounded-pill">add link</button> 
               </div>
             </div>
           </div>
